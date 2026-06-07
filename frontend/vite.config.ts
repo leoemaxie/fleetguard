7ddc07159f0eba6badab54a8054d109788912ff0
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2026-06-07 22:29:36 +0000

    fix: tanstack deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    viteReact(),
+    tanstackStart(),
+    tsconfigPaths(),
   ],
-});
+})
